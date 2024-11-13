b86f0a35ff375d1c8a4d8b2b22bd5090fb34cb57
Stuart Hunt <Stuart.Hunt@eblusolutions.com>
2024-11-13 10:10:19 -0500

    updating my drink service

--- src/services/drink.service.ts
+++ src/services/drink.service.ts
@@ -1,9 +1,28 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Drink } from '../models/drink.model';
+import {API_URL} from "../app/app.constants";
+
+const INIT_DATA = {
+  id: "",
+  name: "",
+  isAlcoholic: "",
+  imageUrl: "",
+  ingredients: "",
+  instructions: "",
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class DrinkService {
 
-  constructor() { }
+  private drinkResults = signal<Drink[]>(INIT_DATA)
+
+  constructor(private readonly http: HttpClient) { }
+
+  getDrinks(): Observable<Drink[]>{
+    return ht
+  }
 }
