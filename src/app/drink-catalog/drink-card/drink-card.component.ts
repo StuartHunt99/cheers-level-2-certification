15324d4dbab52bc77704835d38a22c669dbca41c
Stuart Hunt <Stuart.Hunt@eblusolutions.com>
2024-11-14 06:25:32 -0500

    Added custom pipe, updated styling

--- src/app/drink-catalog/drink-card/drink-card.component.ts
+++ src/app/drink-catalog/drink-card/drink-card.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';  
 import { Drink } from '../../../models/drink.model';
+import { FormatSeparatorPipe } from '../../pipe/format-separator.pipe';
 
 @Component({
   selector: 'app-drink-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormatSeparatorPipe],
   templateUrl: './drink-card.component.html',
   styleUrl: './drink-card.component.scss'
 })
---
+++ src/app/pipe/format-separator.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FormatSeparatorPipe } from './format-separator.pipe';
+
+describe('FormatSeparatorPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FormatSeparatorPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
