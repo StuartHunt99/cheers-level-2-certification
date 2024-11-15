aaa2ce440e00a1878549f6a123394664f3dc8c66
Stuart Hunt <Stuart.Hunt@eblusolutions.com>
2024-11-15 08:47:55 -0500

    added localstorage and favorites services

--- src/app/favorites.service.ts
+++ src/app/favorites.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@angular/core';
+import { effect, inject, Injectable, signal } from '@angular/core';
+import { LocalStorageService } from './local-storage.service';
+import { DrinkId, DrinkIds } from '../models/drink.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,9 +8,32 @@ import { Injectable } from '@angular/core';
 export class FavoritesService {
 
   private readonly FAVORITES_KEY = "favorites";
-  private storage = inject(StorageService<DrinkIds>);
-  private favoritesSignal = signal<DrinkIds>(this.storage.get(this.FAVORITES_KEY));
+  private localStorage = inject(LocalStorageService<DrinkIds>);
+  private favoritesSignal = signal<DrinkIds>(this.localStorage.get(this.FAVORITES_KEY));
   favorites = this.favoritesSignal.asReadonly();
 
-  constructor() { }
+  constructor() { 
+    effect(() => this.localStorage.set(this.FAVORITES_KEY, this.favoritesSignal()));
+  }
+
+  toggleFavorite(drinkId: DrinkId): void {
+    this.favoritesSignal.update(favorites =>
+    favorites.includes(drinkId) 
+      ? favorites.filter(favoriteId => favoriteId !== drinkId)
+      : [...favorites, drinkId]
+    );
+  }
+
+//  ANOTHER OPTION FOR THIS METHOD.  BETTER PERFORMANCE AT THE EXPENSE OF READABILITY;
+//  
+// toggleFavorite(drinkId: DrinkId): void {
+//   const index = this.favoritesSignal().indexOf(drinkId)
+//   if (index !== -1)
+//     this.favoritesSignal.update(favorites => { 
+//     favorites.splice(index, 1);
+//     return [...favorites];
+//   });
+//   else
+//     this.favoritesSignal.update(favorites => [...favorites, drinkId]);
+//   }
 }
