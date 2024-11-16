c3536466a13736b3cd054c970a4542c39696d257
Stuart Hunt <Stuart.Hunt@eblusolutions.com>
2024-11-16 18:55:26 -0500

    Updated elements and styling of drink-details component

--- src/app/drink-details/drink-details.component.ts
+++ src/app/drink-details/drink-details.component.ts
@@ -1,10 +1,13 @@
 import { Component, Input, Signal } from '@angular/core';
 import { Drink } from '../../models/drink.model';
+import { CommonModule } from '@angular/common';
+import { FormatSeparatorPipe } from '../pipe/format-separator.pipe';
+import { ToggleFavoriteDirective } from '../toggle-favorite.directive';
 
 @Component({
   selector: 'app-drink-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormatSeparatorPipe, ToggleFavoriteDirective],
   templateUrl: './drink-details.component.html',
   styleUrl: './drink-details.component.scss'
 })
