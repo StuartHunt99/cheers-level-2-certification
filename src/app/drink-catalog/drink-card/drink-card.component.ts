c7c8523ba6ecd91d6a94446b8c183b7470bab025
Stuart Hunt <Stuart.Hunt@eblusolutions.com>
2024-11-17 07:31:07 -0500

    Changed card title to link

--- src/app/drink-catalog/drink-card/drink-card.component.ts
+++ src/app/drink-catalog/drink-card/drink-card.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { Drink } from '../../../models/drink.model';
 import { PipelineSeparatorPipe } from '../../pipe/pipeline-separator.pipe';
 import { ToggleFavoriteDirective } from '../../toggle-favorite.directive';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-drink-card',
   standalone: true,
-  imports: [CommonModule, PipelineSeparatorPipe, ToggleFavoriteDirective],
+  imports: [CommonModule, PipelineSeparatorPipe, ToggleFavoriteDirective, RouterLink],
   templateUrl: './drink-card.component.html',
   styleUrl: './drink-card.component.scss'
 })
