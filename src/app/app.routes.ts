e28f203e22d9a949d2b3c5b1cd2fbd6e9fc73acf
Stuart Hunt <105444951+StuartHunt99@users.noreply.github.com>
2024-11-18 11:54:46 -0500

    Updated routes and added a not-found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,16 @@ import { RouterModule, Routes } from '@angular/router';
 import { DrinkCatalogComponent } from './components/drink-catalog/drink-catalog.component';
 import { drinkDetailsResolver } from './components/drink-details/drink-details.resolver';
 import { NgModule } from '@angular/core';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: DrinkCatalogComponent },
     { path: "drink/:id",
         loadComponent: () => import("./components/drink-details/drink-details.component").then(m => m.DrinkDetailsComponent),
         resolve: {drink: drinkDetailsResolver}
-      }
+      },
+      {path: 'not-found', component: NotFoundComponent},
+      {path: '**', redirectTo: 'not-found'}
 ];
 
 @NgModule({
